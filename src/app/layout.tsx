9f19ecd5696d89bbf773375ced295cf2f034a655
Filippo <filippopietro.neri@gmail.com>
2024-05-23 00:01:15 +0200

    added metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "LibriMente",
+  title: "LibriMente | La tua libreria online",
 };
 
 export default function RootLayout({
@@ -17,6 +17,20 @@ export default function RootLayout({
     <html lang="en" data-theme="synthwave">
       <head>
         <link rel="icon" href="favicon.ico" sizes="any" />
+        <meta name="title" content="LibriMente | La tua libreria online!" />
+        <meta name="description" content="Sei stanco di tutti i quei libri in giro per casa? Abbiamo la soluzione per te! Con il nostro servizio potrai accedere a più di 10.000+ libri di tutti i generi che vuoi." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://librimente.it/" />
+        <meta property="og:title" content="LibriMente | La tua libreria online!" />
+        <meta property="og:description" content="Sei stanco di tutti i quei libri in giro per casa? Abbiamo la soluzione per te! Con il nostro servizio potrai accedere a più di 10.000+ libri di tutti i generi che vuoi." />
+        <meta property="og:image" content="https://t4.ftcdn.net/jpg/05/81/69/91/360_F_581699110_zG6mpCdtyK0lAvXg89DTrbAFbEdhCrVb.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://librimente.it/" />
+        <meta property="twitter:title" content="LibriMente | La tua libreria online!" />
+        <meta property="twitter:description" content="Sei stanco di tutti i quei libri in giro per casa? Abbiamo la soluzione per te! Con il nostro servizio potrai accedere a più di 10.000+ libri di tutti i generi che vuoi." />
+        <meta property="twitter:image" content="https://t4.ftcdn.net/jpg/05/81/69/91/360_F_581699110_zG6mpCdtyK0lAvXg89DTrbAFbEdhCrVb.jpg" />
       </head>
       <body className="min-h-screen flex flex-col justify-between">
         <Navbar />
